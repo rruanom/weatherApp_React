c859046ec5532b3634a90c9a86a1ab593597f45c
(Robi) <(r.ruano3@gmail.com)>
2024-07-09 17:34:15 +0200

    a falta de darle scss

--- src/components/Main/WeatherList/WeatherCard/WeatherCard.jsx
+++ src/components/Main/WeatherList/WeatherCard/WeatherCard.jsx
@@ -1,7 +1,31 @@
-import React from "react";
+import * as React from 'react';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
 
-const WeatherCard = () => {
-  return <div>WeatherCard</div>;
-};
+export default function WeatherCard({
+  dataItem:{dt_txt, main, weather}}) {
+  return (
+    <Card sx={{ maxWidth: 345 }}>
+      
+      <CardContent>
+      <Typography variant="body2" color="text.secondary">
+          {dt_txt}
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          {weather.description}
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          {main.temp}
+        </Typography>
+      </CardContent>
+      <CardMedia
+        sx={{ height: 140 }}
+        image={`https://openweathermap.org/img/wn/${weather[0].icon}@2x.png`}
+        title={dt_txt}
+      />
+    </Card>
+  );
+}
 
-export default WeatherCard;
--- src/components/Main/WeatherList/WeatherList.jsx
+++ src/components/Main/WeatherList/WeatherList.jsx
@@ -1,7 +1,23 @@
 import React from "react";
+import WeatherCard from './WeatherCard'
+import { v4 as uuidv4 } from 'uuid';
 
-const WeatherList = () => {
-  return <div>WeatherList</div>;
+const WeatherList = ({props, place}) => {
+  console.log(props)
+  const renderItems = () =>
+    props.map((item, i) => (
+      <WeatherCard
+        key={uuidv4()}
+        dataItem={item}
+      />
+    ));
+    const title = `El tiempo en ${place}`
+    return <div>
+    <h1>El tiempo en {place}</h1>
+    <ul>
+      {renderItems()}
+    </ul>
+    </div>
 };
 
 export default WeatherList;
